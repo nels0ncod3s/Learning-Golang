3c934d689a6efd1e150f3bab8d02b386e69f0a3a
Nelson <oluwademiladewey@gmail.com>
2026-07-02 19:33:10 +0100

    Day 2

--- 2.2.go
+++ 2.2.go
@@ -7,6 +7,14 @@ import (
 
 //File reader using defer lol
 func main() {
-    file, err := os.Open(note.txt)
-    
+    file, err := os.Open("note.txt")
+
+    if err != nil{
+        fmt.Println(err)
+        return
+    }
+
+    defer file.Close()
+
+    fmt.Println("File opened successfully")
 }
