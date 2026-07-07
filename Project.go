f16b6118f6cf722b377462575b7b0f34ec53df6d
Nelson <oluwademiladewey@gmail.com>
2026-07-07 14:38:54 +0100

    lol

--- Project.go
+++ Project.go
@@ -6,32 +6,36 @@ import (
 
 var userTask string
 var userAns int
+var taskID int = 0 + 1
+var Tasks = map[int]string{
 
-Taskss := map[int] string{
-	1 : "Do dishes",
 }
 
-func addTask(task string) {
+func addTask() {
 	fmt.Print("ENTER A TASK:")
 	fmt.Scanln(&userTask)
+	Tasks[taskID] = userTask
+	taskID++
 }
 
-func deleteTask(task string) {
+func deleteTask() {
 	//
 }
 
 func listTasks() {
-	fmt.Println(Taskss)
+	fmt.Println(Tasks)
 }
 
 func main (){
 
 	for i := 1; i > 0; i++{
-		fmt.Println("1- Add Task, 2 - End")
+		fmt.Println("1- Add Task, 2 - List Tasks, 3 - End")
 		fmt.Scanln(&userAns)
 		if userAns == 1{
 			addTask()
 		} else if userAns == 2{
+			listTasks()
+		} else if userAns == 3{
 			break
 		} else {
 			fmt.Println("Invalid Command!")
